ae6fe251c485bcdb1f383f52a3d25713676c8503
vuphuong1794 <phuongvu1794@gmail.com>
2025-03-23 20:03:50 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
-  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 
 }
 bootstrap();
