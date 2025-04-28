5bd505c4b16d619337fafb55a46cf55c79b80248
iamnubie <nguyenhongminh952@gmail.com>
2025-04-28 17:49:44 +0700

    get review from user

---
+++ src/review/review.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Review } from 'src/schemas/review.schema';
+
+@Injectable()
+export class ReviewService {
+    constructor(@InjectModel(Review.name) private readonly reviewModel: Model<Review>) { }
+
+    async createReview(body: { userId: string; doctorId: string; rating: number; comment: string }) {
+        const review = new this.reviewModel(body);
+        return review.save();
+    }
+
+    async getReviewsByDoctor(doctorId: string) {
+        return this.reviewModel
+            .find({ doctor: doctorId })
+            .populate('user', 'name userImage')
+            .sort({ createdAt: -1 }); // Mới nhất lên đầu
+    }
+}
