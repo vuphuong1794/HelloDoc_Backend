12ac727c0e6873cdbfa048a3ed62e7cb7dddae22
iamnubie <nguyenhongminh952@gmail.com>
2025-05-01 00:46:32 +0700

    create resource report

---
+++ src/report/report.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { ReportService } from './report.service';
+
+@Controller('report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) { }
+
+  @Post()
+  async create(@Body() body: {
+    reporter: string;
+    reporterModel: 'User' | 'Doctor';
+    content: string;
+    type: 'Bác sĩ' | 'Ứng dụng';
+  }) {
+    return this.reportService.createReport(body);
+  }
+
+  @Get()
+  async getAll() {
+    return this.reportService.getAllReports();
+  }
+
+  @Patch(':id/status')
+  async updateStatus(
+    @Param('id') id: string,
+    @Body('status') status: 'pending' | 'open' | 'closed',
+  ) {
+    return this.reportService.updateStatus(id, status);
+  }
+}
