9cdcc39739f31832d9d9d11249630ccb7af0067c
vuphuong1794 <phuongvu1794@gmail.com>
2025-05-03 18:45:58 +0700

    1/2 minh task

--- src/post-comment/post-comment.service.ts
+++ src/post-comment/post-comment.service.ts
@@ -9,15 +9,15 @@ import { Model } from 'mongoose';
 export class PostCommentService {
 
   constructor(
-          @InjectModel(PostComment.name) private postCommentModel: Model<PostComment>,
-      ) { }
+    @InjectModel(PostComment.name) private postCommentModel: Model<PostComment>,
+  ) { }
 
   async createCommentByPostId(postId: string, createPostCommentDto: CreatePostCommentDto) {
     const createdPostComment = new this.postCommentModel({
-        user: createPostCommentDto.userId,
-        userModel: createPostCommentDto.userModel,
-        post: postId,
-        content: createPostCommentDto.content,
+      user: createPostCommentDto.userId,
+      userModel: createPostCommentDto.userModel,
+      post: postId,
+      content: createPostCommentDto.content,
     });
 
     return createdPostComment.save();
@@ -26,35 +26,38 @@ export class PostCommentService {
 
   async getCommentsByPostId(postId: string) {
     try {
-        const postComments = await this.postCommentModel.find({ post: postId })
-            .populate({
-                path: 'user',
-                select: 'name avatarURL'
-            })
-            .exec();
-        console.error('Post comments:', postComments);
-        const validComments = postComments.filter(comment => comment.user !== null);
-        return validComments;
+      const postComments = await this.postCommentModel.find({ post: postId })
+        .populate({
+          path: 'user',
+          select: 'name avatarURL'
+        })
+        .exec();
+      console.error('Post comments:', postComments);
+      const validComments = postComments.filter(comment => comment.user !== null);
+      return validComments;
     } catch (error) {
-        console.error('Error fetching comments by postId:', error);
-        throw new Error('Không thể lấy danh sách bình luận');
+      console.error('Error fetching comments by postId:', error);
+      throw new Error('Không thể lấy danh sách bình luận');
     }
   }
 
-  // async findAll() {
-  //   return `This action returns all postComment`;
-  // }
-
-  // async findOne(id: number) {
-  //   return `This action returns a #${id} postComment`;
-  // }
-
-  // async update(id: number, updatePostCommentDto: UpdatePostCommentDto) {
-  //   return `This action updates a #${id} postComment`;
-  // }
-
-  // async remove(id: number) {
-  //   return `This action removes a #${id} postComment`;
-  // }
+  async getCommentByUserId(userId: string) {
+    try {
+      const postComments = await this.postCommentModel.find({ user: userId })
+        .populate({
+          path: 'post',
+          select: 'media content',
+        })
+        .populate({
+          path: 'user',
+          select: 'name avatarURL'
+        })
+        .exec();
+      const validComments = postComments.filter(comment => comment.user !== null);
+      return validComments;
+    } catch (error) {
+      throw new Error('Không thể lấy danh sách bình luận');
+    }
+  }
 
 }
