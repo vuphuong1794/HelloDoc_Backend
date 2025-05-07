f81da192e64ad4277f3ac5b948458226553fadaa
Phuc Le <lenguyenminhphuc0706@gmail.com>
2025-05-07 19:51:12 +0700

    adjust module

--- src/post-comment/post-comment.module.ts
+++ src/post-comment/post-comment.module.ts
@@ -3,12 +3,18 @@ import { PostCommentService } from './post-comment.service';
 import { PostCommentController } from './post-comment.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostComment, PostCommentSchema } from 'src/schemas/post-comment.schema';
+import { User, UserSchema } from 'src/schemas/user.schema';
+import { Doctor, DoctorSchema } from 'src/schemas/doctor.schema';
+import { Post, PostSchema } from 'src/schemas/Post.schema';
 import { CacheService } from 'src/cache.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: PostComment.name, schema: PostCommentSchema },
+      { name: Post.name, schema: PostSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Doctor.name, schema: DoctorSchema },
     ])],
   controllers: [PostCommentController],
   providers: [PostCommentService, CacheService],
