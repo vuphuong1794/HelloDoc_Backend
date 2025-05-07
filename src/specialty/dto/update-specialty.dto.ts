09747bc9fa9a789cfd11bdf1c1536798cdd4b6ee
vuphuong1794 <phuongvu1794@gmail.com>
2025-05-07 13:50:04 +0700

    try 4

--- src/specialty/dto/update-specialty.dto.ts
+++ src/specialty/dto/update-specialty.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSpecialtyDto } from './create-specialty.dto';
 import { Express } from 'express';
+import { ObjectId } from 'mongoose';
 
 export class UpdateSpecialtyDto extends PartialType(CreateSpecialtyDto) {
     // Thêm các trường còn thiếu
     image?: Express.Multer.File;
     name?: string;
-    description?: string;
+    doctors?: ObjectId[]; // Danh sách ID bác sĩ thuộc chuyên khoa (nếu có)
 }
