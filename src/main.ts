414b70fecbf538a1dae8385e0b95cbcadfa81075
vuphuong1794 <phuongvu1794@gmail.com>
2025-05-07 14:39:58 +0700

    please work

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,8 @@ async function bootstrap() {
     transform: true
   }));
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
