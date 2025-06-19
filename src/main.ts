2083edc260bcf374e80175b6d184b5c7a1bf6086
MaiKhoa0101 <maikhoa2015@gmail.com>
2025-06-20 03:57:12 +0700

    change update favourite post

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
     transform: true
   }));
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');
   console.log(`Server running on port ${port}`);
 }
